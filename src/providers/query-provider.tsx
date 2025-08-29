9fe80a1080774bcd91434693da38e4222eb2f074
rayaanr <rayaannsn@gmail.com>
2025-08-29 17:33:32 +0530

    fix: Refactor QueryProvider to use state for QueryClient initialization

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function QueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient({
+  const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false, // Avoid refetching on window focus
@@ -16,7 +17,7 @@ export default function QueryProvider({
         retryDelay: 3000,
       },
     },
-  });
+  }));
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
