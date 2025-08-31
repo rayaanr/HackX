2f9dd815bb32f6cad0781d5892f4ce192c6f9902
rayaanr <rayaannsn@gmail.com>
2025-08-31 16:33:50 +0530

    refactor: Simplify button and link structure in QuickActions component for improved readability

--- src/components/dashboard/quick-actions.tsx
+++ src/components/dashboard/quick-actions.tsx
@@ -11,27 +11,27 @@ export function QuickActions() {
       </CardHeader>
       <CardContent>
         <div className="grid gap-3">
-          <Link href="/hackathons/create" className="w-full">
-            <Button className="w-full justify-start" variant="outline">
+          <Button asChild className="w-full justify-start" variant="outline">
+            <Link href="/hackathons/create">
               <Plus className="h-4 w-4 mr-2" />
               Create New Hackathon
-            </Button>
-          </Link>
-          
-          <Link href="/hackathons" className="w-full">
-            <Button className="w-full justify-start" variant="outline">
+            </Link>
+          </Button>
+
+          <Button asChild className="w-full justify-start" variant="outline">
+            <Link href="/hackathons">
               <Calendar className="h-4 w-4 mr-2" />
               View All Events
-            </Button>
-          </Link>
-          
-          <Link href="/projects" className="w-full">
-            <Button className="w-full justify-start" variant="outline">
+            </Link>
+          </Button>
+
+          <Button asChild className="w-full justify-start" variant="outline">
+            <Link href="/projects">
               <Users className="h-4 w-4 mr-2" />
               Manage Projects
-            </Button>
-          </Link>
-          
+            </Link>
+          </Button>
+
           <Button className="w-full justify-start" variant="outline" disabled>
             <Settings className="h-4 w-4 mr-2" />
             Event Settings
