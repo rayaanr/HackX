62a2224896a20996389a95e139a2d216bcdd9e9c
rayaanr <rayaannsn@gmail.com>
2025-09-10 19:29:20 +0530

    Remove: web3 package and related deployment scripts

--- web3/ignition/modules/HackathonFactory.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("HackathonFactoryModule", (m) => {
-  const hackathonFactory = m.contract("HackathonFactory");
-
-  return { hackathonFactory };
-});
