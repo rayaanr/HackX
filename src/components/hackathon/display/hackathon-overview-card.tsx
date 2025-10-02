04bcfbf904eb292cc6797c7a0f6c3d71ea58f20f
rayaanr <rayaannsn@gmail.com>
2025-10-02 23:30:39 +0530

    fix: Adjust image height in HackathonCard for improved layout consistency

--- src/components/hackathon/display/hackathon-overview-card.tsx
+++ src/components/hackathon/display/hackathon-overview-card.tsx
@@ -215,7 +215,7 @@ export function HackathonCard({ hackathon }: HackathonCardProps) {
                 width={300}
                 src={resolveIPFSToHttp(hackathon.visual)}
                 alt={hackathon.name}
-                className="h-40 md:h-full w-full object-cover rounded-lg transform group-hover:scale-[1.03] transition-transform duration-500"
+                className="h-40 md:h-48 w-full object-cover rounded-lg transform group-hover:scale-[1.03] transition-transform duration-500"
                 unoptimized
               />
             </div>
